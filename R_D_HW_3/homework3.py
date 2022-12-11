196bb31c5a85a3c83810d0382c62225f2a62b43a
Valerii Ivanov <deidaravalera@gmai.com>
2022-12-11 23:28:23 +0200

    homework3 is done.

---
+++ R_D_HW_3/homework3.py
@@ -0,0 +1,14 @@
+print('I love Python')
+age_in_month = 12 * 22 + 12 - 5
+age_in_years = age_in_month // 12
+my_age = f"My name is Valery, I'm {age_in_years} years old"
+print(my_age)
+n = 1
+print(n == age_in_years)
+print(n < age_in_years)
+print(n > age_in_month)
+print(age_in_month > n <= age_in_years)
+print(n < 1)
+a, b, c = 2, 5, 6
+d = str(2) + str(5) + str(6)
+
