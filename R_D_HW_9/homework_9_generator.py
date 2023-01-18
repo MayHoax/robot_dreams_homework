27139fdc71e948c450da47810f5f014e2363dd20
Valerii Ivanov <deidaravalera@gmai.com>
2023-01-19 00:14:56 +0200

    code has been changed

--- R_D_HW_9/homework_9_generator.py
+++ R_D_HW_9/homework_9_generator.py
@@ -1,5 +1,5 @@
 def fibonacci_generator():
-    a, b = 0, 1
+    a, b = 1, 1
     while True:
         yield a
         a, b = b, a + b
@@ -10,3 +10,47 @@ while True:
     for i in range(n):
         next_fib = next(fib)
     print(next_fib)
+#
+
+# lst = [1, 'I', 'am', 2, 5, 'list']
+# lst_iter = iter(lst)
+# helper = len(lst)
+# while helper != 0:
+#     print(next(lst_iter))
+#
+#
+# class Itario:
+#     value = 1
+#     def __iter__(self):
+#         return self
+#     def __next__(self):
+#         if self.value == 10:
+#             raise StopIteration()
+#         tmp_value = self.value
+#         self.value += 1
+#         return tmp_value
+#
+# maza = Itario()
+# for i in maza:
+#     print(i)
+#     print('okay, whats next')
+#
+# class FibonacciIterator:
+#     def __init__(self, limit):
+#         self.limit = limit
+#         self.a = 0
+#         self.b = 1
+#
+#     def __iter__(self):
+#         return self
+#
+#     def __next__(self):
+#         fib = self.a
+#         if fib > self.limit:
+#             raise StopIteration
+#         self.a, self.b = self.b, self.a + self.b
+#         return fib
+#
+# fib_iterator = FibonacciIterator(100)
+# for fib in fib_iterator:
+#     print(fib)
