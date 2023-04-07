dc7366c8ff194491627d929ee45da042f2a79947
Valerii Ivanov <deidaravalera@gmai.com>
2023-04-07 10:31:05 +0300

    Homework 30 is done after changes required

--- R_D_HW_30/API_2.py
+++ R_D_HW_30/API_2.py
@@ -3,22 +3,7 @@ import random
 import json
 
 
-while True:
-    city = input("City name:")
-    coordinates = "https://geocoding-api.open-meteo.com/v1/search?name="
-    response = requests.get(f'{coordinates}{city}')
-    response_dict = json.loads(response.text)
-    city_latitude = round(response_dict.get('results')[0]['latitude'], 2)
-    city_longitude = round(response_dict.get('results')[0]['longitude'], 2)
-    timezone = "Europe/Moscow"
-    weather_url = f"https://api.open-meteo.com/v1/forecast?latitude={city_latitude}" \
-                        f"&longitude={city_longitude}&timezone={timezone}&current_weather=true"
-
-    weather = json.loads(requests.get(weather_url).text)
-    temperature = weather.get('current_weather')['temperature']
-    windspeed = weather.get('current_weather')['windspeed']
-    weathercode = weather.get('current_weather')['weathercode']
-    weathercode_dict = weather_dict = {
+weathercode_dict = {
     0: 'Ясное небо',
     1: 'Преимущественно ясно',
     2: 'Частичная облачность',
@@ -49,6 +34,41 @@ while True:
     99: 'Гроза с сильным градом'
 }
 
-    print(f' Температура: {temperature}, Скорость ветра {windspeed}, {weathercode_dict.get(weathercode)}')
+
+def get_weather_url(latitude, longitude, timezone):
+    return f"https://api.open-meteo.com/v1/forecast?latitude={latitude}" \
+           f"&longitude={longitude}&timezone={timezone}&current_weather=true"
+
+while True:
+    city = input("City name:")
+    coordinates = "https://geocoding-api.open-meteo.com/v1/search?name="
+    try:
+        response_dict = requests.get(f'{coordinates}{city}').json()
+        city_latitude = round(response_dict.get('results')[0]['latitude'], 2)
+        city_longitude = round(response_dict.get('results')[0]['longitude'], 2)
+        timezone = "Europe/Kiev"
+        weather_url = get_weather_url(city_latitude, city_longitude, timezone)
+
+        weather = json.loads(requests.get(weather_url).text)
+        temperature = weather.get('current_weather')['temperature']
+        wind_speed = weather.get('current_weather')['windspeed']
+        weather_code = weather.get('current_weather')['weathercode']
+
+        print(f' Температура: {temperature}, Скорость ветра {wind_speed}, {weathercode_dict.get(weather_code)}')
+    except (Exception):
+        print('No city with that name')
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
