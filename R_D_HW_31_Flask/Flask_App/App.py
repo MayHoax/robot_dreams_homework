5dd8e331eff1a3d2f20b164ac238a86bed594474
Valerii Ivanov <deidaravalera@gmai.com>
2023-04-10 16:30:45 +0300

    Homework 31 is done

--- R_D_HW_31_Flask/Flask_App/App.py
+++ R_D_HW_31_Flask/Flask_App/App.py
@@ -2,6 +2,12 @@ from flask import Flask, logging
 import logging
 from logging.config import dictConfig
 
+dictConfig({
+    'version': 1,
+    'formatters': {'default': {
+        'format': '%(levelname)s %(message)s',
+    }}
+})
 
 app = Flask(__name__)
 logger = logging.getLogger('First_logger_ever')
